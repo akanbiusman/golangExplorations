fb975731a224d58dfea23daa24fdfcb52c9a697c
usman <akanbiusman5@gmail.com>
2025-11-13 23:41:20 +0100

    golang learnings

---
+++ go techworld with nana/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	remainingTickets := 50
+
+	fmt.Printf("ConferenceTickets is %T, remainingTickets is %T, conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to the %v \n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still remaining \n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets", userName, userTickets)
+}
