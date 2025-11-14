890aede0559e18dce3405b1b5fd68a097f47d54f
usman <akanbiusman5@gmail.com>
2025-11-14 23:05:13 +0100

    conditional statements update

--- go techworld with nana/main.go
+++ go techworld with nana/main.go
@@ -37,19 +37,32 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
 
-		// bookings[0] = firstName + " " + lastName  //adding a value into a fixed size array
-		bookings = append(bookings, firstName+" "+lastName) //adding a value into a slice
+			// bookings[0] = firstName + " " + lastName  //adding a value into a fixed size array
+			bookings = append(bookings, firstName+" "+lastName) //adding a value into a slice
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+
+			// noTicketsRemaining := remainingTickets == 0
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets.\n", remainingTickets, userTickets)
+			continue
 		}
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 	}
+
 }
