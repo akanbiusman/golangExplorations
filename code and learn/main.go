6b0a8fdc1a5ceb98cb89dcc6cf3de3e2d38e797f
usman <akanbiusman5@gmail.com>
2025-11-28 23:58:06 +0100

    structs

--- code and learn/main.go
+++ code and learn/main.go
@@ -3,23 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	a := squareNum(5)
-	fmt.Println(a)
+	type student struct {
+		firstName string
+		lastName  string
+		age       int
+		classes   []string
+	}
 
-	b := make([]int, 5)
-	b[1] = 10
+	foo := student{
+		firstName: "usman",
+		lastName:  "akanbi",
+		age:       10,
+	}
 
-	fmt.Println(b)
-
-	c := appendSlice([]int{1, 2, 3})
-	fmt.Println(c)
-}
-
-func squareNum(num int) int {
-	return num * num
-}
-
-func appendSlice(x []int) []int {
-	x = append(x, 11)
-	return x
+	fmt.Println(foo.lastName)
 }
