43d396d85f8f36c1202dd8d67049955f4a3a9e35
usman <akanbiusman5@gmail.com>
2025-11-28 23:11:34 +0100

    slices

--- code and learn/main.go
+++ code and learn/main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var a = [2][2]int{{1, 2}, {3, 4}}
-	fmt.Println(a[1][1])
+	a := squareNum(5)
+	fmt.Println(a)
 
-	a[1][1] = 5
-	fmt.Println(a[1][1])
+	b := make([]int, 5)
+	b[1] = 10
+
+	fmt.Println(b)
+
+	c := appendSlice([]int{1, 2, 3})
+	fmt.Println(c)
+}
+
+func squareNum(num int) int {
+	return num * num
+}
+
+func appendSlice(x []int) []int {
+	x = append(x, 11)
+	return x
 }
