a780b908f41ab2f223fb016e04ed3e720b679dff
usman <akanbiusman5@gmail.com>
2025-11-29 21:45:20 +0100

    book

--- code and learn/main.go
+++ code and learn/main.go
@@ -1,20 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Book struct {
+	title    string
+	author   string
+	pages    int
+	flips    int
+	isSaved  bool
+	isRented bool
+	readTime time.Time
+}
 
 func main() {
-	type student struct {
-		firstName string
-		lastName  string
-		age       int
-		classes   []string
+	gow := Book{
+		title:    "god of war",
+		author:   "usman",
+		pages:    10,
+		flips:    3,
+		isSaved:  false,
+		isRented: false,
+		readTime: time.Now(),
 	}
 
-	foo := student{
-		firstName: "usman",
-		lastName:  "akanbi",
-		age:       10,
-	}
+	saveBook(&gow)
+	fmt.Println(gow.isSaved)
+	fmt.Println(gow.readTime)
+	flipBook(&gow)
+}
 
-	fmt.Println(foo.lastName)
+func flipBook(book *Book) {
+	for i := 0; i < book.pages; i++ {
+		if i == book.flips {
+			pagesLeft := book.pages - book.flips
+			fmt.Printf("You have %v pages left to read", pagesLeft)
+		}
+	}
+}
+
+func saveBook(book *Book) {
+	book.isSaved = true
+	book.readTime = time.Now()
 }
