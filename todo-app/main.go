a2142db65e124c81d29c0041753587439114d28b
usman <akanbiusman5@gmail.com>
2025-12-05 00:13:20 +0100

    storage and command

--- todo-app/main.go
+++ todo-app/main.go
@@ -2,6 +2,8 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.add("Buy milk")
 	todos.add("Buy bread")
 	todos.add("Buy beans")
@@ -9,4 +11,5 @@ func main() {
 	todos.print()
 	todos.delete(1)
 	todos.print()
+	storage.save(todos)
 }
